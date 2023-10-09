ec532065618253771394b45ac2651dd6485ed6fa
millifail <hwangbird99@gmail.com>
2023-10-10 01:47:56 +0900

    이미지 포스팅 및 세팅

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,16 @@
+
+
 from django.urls import path
+from django.urls import include
+from rest_framework import routers
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token
+
+router = routers.DefaultRouter()
+router.register('Post', views.IntruderImage)
+
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path("", views.post_list, name="post_list"),
+    path('api_root/', include(router.urls)),
+    path('api-token-auth/', obtain_auth_token),
 ]
--- mysite/settings.py
+++ mysite/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,15 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media/"
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        # 'rest_framework.permissions.IsAdminUser’,
+    ],
+    'PAGE_SIZE': 10
+}
